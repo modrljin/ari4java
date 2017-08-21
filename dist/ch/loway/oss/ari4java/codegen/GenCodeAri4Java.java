972f9750e61ddf81dadeed9016c67d47d413a8d2
modrljin <marin@maxweb1>
2017-08-21 16:15:41 +0200

    Changed gitignore file

---
+++ dist/ch/loway/oss/ari4java/codegen/GenCodeAri4Java.java
@@ -0,0 +1,54 @@
+package ch.loway.oss.ari4java.codegen;
+
+import java.io.File;
+import java.io.IOException;
+
+import ch.loway.oss.ari4java.codegen.DefMapper;
+
+/**
+ * @author lenz (modified by modrljin)
+ */
+public class GenCodeAri4Java {
+
+	public static String PROJECT = "D:/Projects/_GitHub/ari4java-MOdrljin";
+    
+    public static String SOURCES = PROJECT + "/codegen-data/";
+
+    public static void main( String [] argv ) throws IOException {
+
+        System.out.println("This is ARI4JAVA Code Generator version " + VERSION.VER);
+
+        DefMapper dm = new DefMapper();
+        dm.setProjectFolder(PROJECT);
+
+        loadAsteriskDefs( dm, "ari_0_0_1" );
+        loadAsteriskDefs( dm, "ari_1_0_0" );
+        loadAsteriskDefs( dm, "ari_1_5_0" );
+        loadAsteriskDefs( dm, "ari_1_6_0" );
+        loadAsteriskDefs( dm, "ari_1_7_0" );
+        loadAsteriskDefs( dm, "ari_1_8_0" );
+        loadAsteriskDefs( dm, "ari_1_9_0" );
+        loadAsteriskDefs( dm, "ari_2_0_0" );
+        
+        dm.generateAllClasses();
+        
+        //dm.writeProperties("ari_0_0_1");
+    }
+
+    private static void loadAsteriskDefs( DefMapper dm, String srcVer ) throws IOException {
+
+        String srcDir = SOURCES + srcVer + "/";
+
+        dm.clean( srcVer );
+        dm.parseJsonDefinition( new File(srcDir + "applications.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "asterisk.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "bridges.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "channels.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "endpoints.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "playbacks.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "recordings.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "sounds.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "deviceStates.json"), srcVer, false );
+        dm.parseJsonDefinition( new File(srcDir + "events.json"), srcVer, true );
+    }
+}
